a7dd5d5d0bc7a53069284940b8b306e43068f21d
lucky <luckyparashar641@gmail.com>
2022-09-17 09:18:49 +0530

    second

---
+++ clock.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from tkinter.ttk import *
+
+from time import strftime
+
+root = Tk()
+root.title("clock")
+
+def time():
+    string = strftime('%H:%M:%S %p')
+    lable.config(text=string)
+    lable.after(1000, time)
+
+lable = Label(root, font=("ds-digital",80), background = "cyan", foreground = "red") 
+lable.pack(anchor='center')
+time()
+mainloop()   
